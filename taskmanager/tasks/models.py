289e43a1928b854369a1d9da2b1cac043a365749
gomugomucode <alinedtwins@gmail.com>
2025-06-25 19:36:02 +0545

    adding minimal styling to webpage sections

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -4,11 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 # creating the task model here
 
+  
 class Task(models.Model):
+    STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    completed = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     due_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
